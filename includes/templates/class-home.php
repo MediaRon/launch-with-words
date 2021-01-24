eee44cfb2517b45345e39b3244e9ad2023db7b07
Ronald Huereca <rhuereca@updraftplus.com>
2021-01-24 05:58:37 -0600

    Home screen tweaks for Launch With Words.

--- includes/templates/class-home.php
+++ includes/templates/class-home.php
@@ -19,7 +19,7 @@ class Home {
 	public function __construct() {
 		add_filter( 'lww_admin_tabs', array( $this, 'add_home_tab' ), 1, 1 );
 		add_filter( 'lww_admin_sub_tabs', array( $this, 'add_home_main_sub_tab' ), 1, 3 );
-		add_filter( 'lww_output_dashboard', array( $this, 'output_home_content' ), 1, 3 );
+		add_filter( 'lww_output_home', array( $this, 'output_home_content' ), 1, 3 );
 	}
 
 	/**
@@ -60,7 +60,14 @@ class Home {
 		if ( 'home' === $tab ) {
 			if ( empty( $sub_tab ) || 'home' === $sub_tab ) {
 				?>
-				<div id="lww-home-options"></div>
+				<div id="lww-home-options">
+					<h3>Your clients want to blog. But they don't know what to write. Let us show you how.</h3>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/kXqwqIxy1u8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<hr />
+					<blockquote>
+						Launch With Words is a new WordPress plugin that combines the features of a business coach with blogging prompts to encourage you to publish once a month.
+					</blockquote>
+				</div>
 				<?php
 			}
 		}
