c58b64f79c5f276a6a5a0efa919ba6c53a29bb23
Ronald Huereca <rhuereca@updraftplus.com>
2021-01-23 19:25:54 -0600

    Launch with words base admin style.

--- includes/templates/class-init.php
+++ includes/templates/class-init.php
@@ -19,5 +19,29 @@ class Init {
 	public function run() {
 		new \LWW\Includes\Templates\Home();
 		new \LWW\Includes\Templates\Import();
+
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+	}
+
+	/**
+	 * Register scripts and styles for the admin panel.
+	 */
+	public function enqueue() {
+		// Make sure we enqueue on the right admin screen.
+		$screen = get_current_screen();
+		if ( ! isset( $screen->id ) ) {
+			return;
+		}
+		if ( 'posts_page_launch-with-words' !== $screen->id ) {
+			return;
+		}
+
+		wp_enqueue_style(
+			'lww-admin',
+			Functions::get_plugin_url( '/dist/admin-panel.css' ),
+			array(),
+			Functions::get_plugin_version(),
+			'all'
+		);
 	}
 }
